be6afa24bd2b1da0b66a92d9b811b649d2899bbe
Jerry Hilts <gwhilts@gmail.com>
2016-06-04 18:39:07 -0700

    Re-implements R. Numeral kata
    Read a blog post by Sandi Metz where she discused breaking the
    Roman Numeral conversion into two steps, first building up the
    without worying about 4, 9, 40, et al., then replacing the long
    strings ("IIII") with the shorts ("IV").
    
    Trying this approach, let me reduce the long series of brute-force
    conditionals into two simple methods.

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,50 +1,43 @@
 class RomanNumerals
+  ROMANS = {
+    1000 => 'M',
+    500  => 'D',
+    100  => 'C',
+    50   => 'L',
+    10   => 'X',
+    5    => 'V', 
+    1    => 'I'
+  }
+
+  ROMAN_SHORTS = {
+    'DCCCC' => 'CM',
+    'CCCC'  => 'CD',
+    'LXXXX' => 'XC',
+    'XXXX'  => 'XL',
+    'VIIII' => 'IX',
+    'IIII'  => 'IV'
+  }
+
   def romanize(arabic)
-    pull_digits(arabic, "")
+    compact( build_up(arabic) )
   end
 
   private
 
-  def pull_digits(arabic, roman)
-    case arabic
-    when 900..999
-      roman << "CM"
-      pull_digits (arabic.remainder 900), roman
-    when 500..899
-      roman << "D"
-      pull_digits (arabic.remainder 500), roman
-    when 400..499
-      roman << "CD"
-      pull_digits (arabic.remainder 400), roman
-    when 100..399
-      roman << "C" * (arabic/100)
-      pull_digits (arabic.remainder 100), roman
-    when 90..99
-      roman << "XC"
-      pull_digits (arabic.remainder 90), roman
-    when 50..89
-      roman << "L" 
-      pull_digits (arabic.remainder 50), roman
-    when 40..49
-      roman << "XL" 
-      pull_digits (arabic.remainder 40), roman
-    when 10..39
-      roman << "X" * (arabic/10)
-      pull_digits (arabic.remainder 10), roman
-    when 9
-      roman << "IX"
-    when 5..8
-      roman << "V"
-      pull_digits arabic - 5, roman
-    when 4
-      roman << "IV"
-    when 1..3
-      roman << "I" * arabic
-    when 0
-      roman
-    else
-      roman << "M" * (arabic/1000)
-      pull_digits (arabic.remainder 1000), roman
+  def build_up(number)
+    roman = ''
+    ROMANS.keys.reduce(number) do |num, key|
+      reps, next_num = num.divmod key
+      roman << ROMANS[key] * reps
+      next_num
+    end
+    roman
+  end
+
+  def compact(roman)
+    ROMAN_SHORTS.keys.reduce(roman) do |big_roman, r_digits|
+      big_roman.gsub( r_digits, ROMAN_SHORTS[r_digits] )
     end
   end
+
 end
