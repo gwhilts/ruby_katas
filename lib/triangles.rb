e11fe6c654294d573a4c4fec6ead575624fad801
Jerry Hilts <gwhilts@gmail.com>
2016-06-11 13:42:59 -0700

    Triangle Factors: Improves speed performance
    My original solution used a simple, brute-force algorithm
    to find all the factors of each triangle number: iterate
    from 1 to sqrt of t, check for % == 0. This worked when
    T was relatively small, but began to choke when searching for
    T numbers with more than a hundred factors.
    
    I replaced the simple factoring expression with a method that
    begins with the prime factors (via Prime.prime_division), then
    builds up the non-prime factors from there.
    
    The previous version failed to find a T number with 500 factors
    after more than 2 hours.  The current version finds it in less
    than 2 milliseconds.

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -1,12 +1,24 @@
 class Triangles
+  require 'prime'
+
   def self.first_with_factor_count(c)
     factors = []
-    t = n = 0
-    while factors.count < c do
+    t = n = factors = 1
+    while factors < c do
       n += 1  
       t = t + n  
-      factors = (1..t).select { |f| (t % f).zero? }  
+      factors = factors_of(t).count
     end  
     t
   end
+
+
+  def self.factors_of(n)
+    primes, powers = n.prime_division.transpose
+    exponents = powers.map{|i| (0..i).to_a}
+    factors = exponents.shift.product(*exponents).map do |exp|
+      primes.zip(exp).map{|prime, power| prime ** power}.inject(:*)
+    end
+  end
+
 end
--- test/triangles_test.rb
+++ test/triangles_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 require 'triangles'
+require 'benchmark'
 
 class TrianglesTest < Minitest::Test
   describe 'Triangles.first_with_factor_count' do
@@ -8,5 +9,11 @@ class TrianglesTest < Minitest::Test
       ( Triangles.first_with_factor_count  5 ).must_equal 28
       ( Triangles.first_with_factor_count 10 ).must_equal 120
     end
+
+   it 'works quickly with really big numbers' do
+     # Find the 1st triangle with 500 factors in < 3 seconds?
+     ( Benchmark.realtime { Triangles.first_with_factor_count 500 } ).to_i.must_be_close_to 1, 1
+   end
+
   end
 end
