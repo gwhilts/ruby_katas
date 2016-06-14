359c7e14dc6c273da6adea46f5d57ea4161bcd50
Jerry Hilts <gwhilts@gmail.com>
2016-06-13 19:44:32 -0700

    Palindrome Numbers: Tried another implementation, but too slow.
    Tried a different implementation of the .find_largest_product method
    that used Enum::product to produce all the number pairs rather than
    nested iterations (while, Fixnum#downto). Result was a a much
    clearer, smaller, more expressive method, but it also ran about 3x
    slower.
    
    Reverted to the old code, but left in the new code, commented out.

--- lib/palindrome_numbers.rb
+++ lib/palindrome_numbers.rb
@@ -1,5 +1,18 @@
 class PalindromeNumbers
   def self.find_largest_product(high, low = 1)
+
+# This version is somewhat clearer than that below, but is about 3x slower than that below
+
+#   pairs       = (low..high).to_a.product (low..high).to_a
+#   products    = pairs.map { |p| p[0] * p[1] }
+#   palindromes = products.select { |p| p.to_s == p.to_s.reverse }
+#   palindromes.max
+
+
+# This version is much faster (~3x) than the one above, but
+# could probably be even faster, if stop the countdown when
+# when a palidrome is found, then decrement the high value.
+
     palindromes = []
     while high > low
       high.downto low do |n|
@@ -12,4 +25,6 @@ class PalindromeNumbers
     end
     palindromes.max
   end
+
+
 end
