7ec4fefb1e8f4e99d37f23a3da34c3c599db5feb
Jerry Hilts <gwhilts@gmail.com>
2016-06-28 22:03:06 -0700

    Euler: Solves Problem 29
    Determines the number of distinct powers in a**b, for all permutations
    where a and b represent the set of integers from 2 to 100.
    
    Implemented two methods to calculate the solution: JMath.power_permutation(range)
    and JMath.distinct_powers(range).
    
    Very happy with this solution. It uses only two single-line methods
    that are relatively clear and runs in ~ 0.02 seconds.

--- lib/j_math.rb
+++ lib/j_math.rb
@@ -19,5 +19,14 @@ class JMath
         nil
       end
     end
+
+    def power_permutations(range)
+      range.to_a.permutation(2).to_a + range.map { |n| [n, n] } 
+    end
+
+    def distinct_powers(range)
+      ( ( power_permutations range ).map { |b, p| b**p } ).uniq
+    end
+
   end
 end
