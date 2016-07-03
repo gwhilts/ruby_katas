8c62a2eac99c6768e7bba2e41f050015f0569e76
Jerry Hilts <gwhilts@gmail.com>
2016-07-03 11:33:17 -0700

    Extracts GRID constant into GridData class

--- test/grid_test.rb
+++ test/grid_test.rb
@@ -1,10 +1,11 @@
 require 'test_helper'
 require 'grid'
+require 'grid_data'
 
 describe Grid do
   describe ".largest_product" do
     it "calculates the largest product of 4 adjacent (horizontal, vertical, diagonal) number in given grid" do
-      Grid.largest_product.must_equal 70_600_674
+      Grid.largest_product(GridData::Euler).must_equal 70_600_674
     end
   end
 end
