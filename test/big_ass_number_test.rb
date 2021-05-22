2f5343a47baa227016575f6a414e7763de112813
Gerald Hilts <gwhilts@booknowl.com>
2021-05-22 16:44:50 -0700

    BigAssNumber: Update test
    minitest-spec now requires expectation subject
    to be wrapped in _( ).

--- test/big_ass_number_test.rb
+++ test/big_ass_number_test.rb
@@ -4,9 +4,9 @@ require 'big_ass_number'
 class BigAssNumberTest < Minitest::Test
   describe 'BigAssNumber.largest_product_of' do
     it 'the largest product of adjacent of the given length' do
-      ( BigAssNumber.largest_product_of  2 ).must_equal 81
-      ( BigAssNumber.largest_product_of  4 ).must_equal 5832
-      ( BigAssNumber.largest_product_of 13 ).must_equal 23_514_624_000
+      _( BigAssNumber.largest_product_of  2 ).must_equal 81
+      _( BigAssNumber.largest_product_of  4 ).must_equal 5832
+      _( BigAssNumber.largest_product_of 13 ).must_equal 23_514_624_000
     end
   end
 end
