f5b3fa707b71b450e4dde45de6ac9bfc5c43f7a9
Gerald Hilts <gwhilts@booknowl.com>
2021-05-22 16:44:03 -0700

    Anagrams: require Set / upate unit test
    - Need to explicitely Set
    - minitest-spec now requires expectation subject
    to be wrapped in _( ).

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 class Anagrams
   attr_reader :dict
 
--- test/anagrams_test.rb
+++ test/anagrams_test.rb
@@ -6,15 +6,15 @@ describe Anagrams do
   
   describe "#add_words" do
     it "adds words to the anagram dictionary" do
-      ( ags.dict.keys.count ).must_equal 6
+      _( ags.dict.keys.count ).must_equal 6
       ags.add_words %w{ chore ochre atm }
-      ( ags.dict.keys.count ).must_equal 7
+      _( ags.dict.keys.count ).must_equal 7
     end
   end
 
   describe "#find_for" do
     it "finds anagaams for a given word" do
-      ( ags.find_for 'rat' ).must_equal ['art', 'rat', 'tar']
+      _( ags.find_for 'rat' ).must_equal ['art', 'rat', 'tar']
     end
 
     it "deals with accented characters" do
