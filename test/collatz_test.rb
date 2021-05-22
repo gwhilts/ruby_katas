7bb7b9892a00838f13f3d53ae02b7ed3a2339e10
Gerald Hilts <gwhilts@booknowl.com>
2021-05-22 16:45:44 -0700

    Collatz: Update unit test
    minitest-spec now requires expectation subject
    to be wrapped in _( ).

--- test/collatz_test.rb
+++ test/collatz_test.rb
@@ -4,8 +4,8 @@ require 'collatz'
 class CollatzTest < Minitest::Test
   describe "Collatz.sequence_from(start)" do
     it "returns the Collatz from the given number down to 1" do
-      ( Collatz.sequence_from 13 ).must_equal [13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
-      ( Collatz.sequence_from 52 ).must_equal [52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
+      _( Collatz.sequence_from 13 ).must_equal [13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
+      _( Collatz.sequence_from 52 ).must_equal [52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
     end
   end
 
@@ -13,8 +13,8 @@ class CollatzTest < Minitest::Test
     it "returns the starting number and length of the longest sequence begining with a number < or = to the given limit" do
       expected = Collatz.longest_under 100
       
-      (expected[:start]  ).must_equal  97
-      (expected[:length] ).must_equal 119
+      _(expected[:start]  ).must_equal  97
+      _(expected[:length] ).must_equal 119
     end
   end
 end
