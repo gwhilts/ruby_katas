6fbf4dedf0ad6c0a794e4fb5b21f2721aa9eee5b
Gerald Hilts <gwhilts@booknowl.com>
2021-05-22 16:45:55 -0700

    Fib: Update unit test
    minitest-spec now requires expectation subject
    to be wrapped in _( ).

--- test/fib_test.rb
+++ test/fib_test.rb
@@ -6,8 +6,8 @@ class FibTest < MiniTest::Test
   describe "Fib.fib(n)" do
     it "calculates the Nth number of the Fibonacci sequence" do
       # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233
-      Fib.fib(4).must_equal 2
-      Fib.fib(9).must_equal 21
+      _(Fib.fib 4).must_equal 2
+      _(Fib.fib 9).must_equal 21
     end
   end
 
@@ -18,16 +18,16 @@ class FibTest < MiniTest::Test
     # limit = 200
     # 2 + 8 + 34 + 144 = 188
     it "adds even Fibacci numbers" do
-      Fib.sum_of_evens_to(8).must_equal 10
-      Fib.sum_of_evens_to(100).must_equal 44
-      Fib.sum_of_evens_to(200).must_equal 188
+      _(Fib.sum_of_evens_to 8).must_equal 10
+      _(Fib.sum_of_evens_to 100).must_equal 44
+      _(Fib.sum_of_evens_to 200).must_equal 188
     end
   end
 
   describe "Fib.index_where_digit_count(count)" do
     it "finds the index of the first fibonacci number with the given number of digits" do
-      Fib.index_where_digit_count(3).must_equal 12
-      Fib.index_where_digit_count(100).must_equal 476
+      _(Fib.index_where_digit_count 3).must_equal 12
+      _(Fib.index_where_digit_count 100).must_equal 476
     end
 
     it "works quickly with very large numbers" do
