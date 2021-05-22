54c073d50efa6636926d3c304e543d5c7024aa7f
Gerald Hilts <gwhilts@booknowl.com>
2021-05-22 16:45:06 -0700

    CircularPrimes: Update test
    minitest-spec now requires expectation subject
    to be wrapped in _( ).

--- test/circular_primes_test.rb
+++ test/circular_primes_test.rb
@@ -5,19 +5,19 @@ describe CircularPrimes do
 
   describe ".primes_to(limit)" do
     it "returns all the primes up to the given limit" do
-      (CircularPrimes.primes_to 10).must_equal [2, 3, 5, 7]
+      _(CircularPrimes.primes_to 10).must_equal [2, 3, 5, 7]
     end
   end
 
   describe ".circulars_primes_to(limit)" do
     it "returns the circular primes below the given limit" do
-      CircularPrimes.circular_primes_to(100).must_equal [2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97]
+      _(CircularPrimes.circular_primes_to 100).must_equal [2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97]
     end
   end
 
   describe ".euler" do
     it "correctly determines the number of circular primes below 1_000_000" do
-      CircularPrimes.euler.must_equal 55
+      _(CircularPrimes.euler).must_equal 55
     end
   end
 end
